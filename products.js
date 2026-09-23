8c2191cdfa3e3980560af462365462e61023d4e8
skt720981-blip <skt720981@gmail.com>
2026-09-23 07:38:57 +0530

    Update products.js

--- products.js
+++ products.js
@@ -41,7 +41,7 @@ function confirmBuy(){
 
   localStorage.setItem("pendingProduct", JSON.stringify(selected));
 
-  window.location.href =window.location.href = "payment.html?product=" + encodeURIComponent(selected.name) + "&amount=" + selected.price;
+  window.location.href = "payment.html?product=" + encodeURIComponent(selected.name) + "&amount=" + selected.price;
 }
 
 render();
